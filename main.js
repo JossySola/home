81c61a68af9ea1e53b4027f9c054ead4b46c0f71
Jossy Solá <jossysola@gmail.com>
2022-05-02 11:45:24 -0500

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@
 import { Desktop, lan, device, getClick, mainDesktop, getSelection } from "./resources/modules/classes.js";
 const {width, orientation, touch, mobile} = device;
 
-if (device.touch > 0) {
+if ( window.navigator.maxTouchPoints > 0 ) {
     // SET MAIN DESKTOP ENVIRONMENT
     const desk = new Desktop('desktop');
     mainDesktop.push(desk);
